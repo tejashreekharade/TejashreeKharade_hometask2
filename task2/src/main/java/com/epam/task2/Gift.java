36842c96ab1f359ce32d0edb6bb67cc44ec07942
tejashreekharade <47024628+tejashreekharade@users.noreply.github.com>
2020-07-26 15:57:29 +0530

    First Commit

---
+++ task2/src/main/java/com/epam/task2/Gift.java
@@ -0,0 +1,35 @@
+package com.epam.task2;
+
+
+public class Gift {
+	int weight;
+	int sweetness;
+	String name;
+
+	public Gift(int weight, int sweetness, String name) {
+		super();
+		this.weight = weight;
+		this.sweetness = sweetness;
+		this.name = name;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getSweetness() {
+		return sweetness;
+	}
+	public void setSweetness(int sweetness) {
+		this.sweetness = sweetness;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
